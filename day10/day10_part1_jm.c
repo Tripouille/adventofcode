36ce4263efbc4d7c73f2dd4746388b32fa2778e1
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-12-22 16:08:42 +0100

    Day 10 part 1 (I FEEL IT IN MY ...)

---
+++ day10/day10_part1_jm.c
@@ -0,0 +1,132 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   day10_part1_jm.c                                 .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/10 11:27:17 by jgambard     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/22 15:40:00 by jgambard    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+
+int		get_next_line(int fd, char **line);
+
+char	*map[100];
+int		map_w;
+int		map_h;
+
+int		already_view(double t, double *tan)
+{
+	while (*tan)
+		if (*tan++ == t)
+			return (1);
+	return (0);
+}
+
+int		count_up(int y, int x)
+{
+	double	tan[500] = {0};
+	double	t;
+	int		i;
+	int		checked_y;
+	int		checked_x;
+
+	i = 0;
+	checked_y = -1;
+	while (++checked_y < y)
+	{
+		checked_x = -1;
+		while (++checked_x < map_w)
+		{
+			t = ((double)y - checked_y) / ((double)x - checked_x);
+			if (map[checked_y][checked_x] == '#' && !already_view(t, tan))
+				tan[i++] = t;
+		}
+	}
+	return (i);
+}
+
+int		count_down(int y, int x)
+{
+	double	tan[500] = {0};
+	double	t;
+	int		i;
+	int		checked_y;
+	int		checked_x;
+
+	i = 0;
+	checked_y = y;
+	while (++checked_y < map_h)
+	{
+		checked_x = -1;
+		while (++checked_x < map_w)
+		{
+			t = ((double)y - checked_y) / ((double)x - checked_x);
+			if (map[checked_y][checked_x] == '#' && !already_view(t, tan))
+				tan[i++] = t;
+		}
+	}
+	return (i);
+}
+
+int		count_views(char **map, int y, int x)
+{
+	int		views;
+	int		i;
+
+	views = count_up(y, x);
+	views += count_down(y, x);
+	i = -1;
+	while (++i < x && map[y][i] != '#');
+	views += i < x ? 1 : 0;
+	i = x;
+	while (++i < map_w && map[y][i] != '#');
+	views += i < map_w ? 1 : 0;
+	return (views);
+}
+
+int		main(int ac, char **av)
+{
+	char	*line;
+	int		y;
+	int		x;
+	int		views;
+	int		max_views;
+
+	if (ac != 2)
+		return (0);
+	int fd = open(av[1], O_RDONLY);
+	map_h = 0;
+	while (get_next_line(fd, &line) == 1)
+		map[map_h++] = line;
+	map_w = 0;
+	while (map[0][map_w])
+		map_w++;
+	y = -1;
+	max_views = 0;
+	while (++y < map_h)
+	{
+		x = -1;
+		while (++x < map_w)
+		{
+			if (map[y][x] == '#')
+			{
+				views = count_views(map, y, x);
+				if (views > max_views)
+					max_views = views;
+			}
+		}
+	}
+	printf("result = %d\n", max_views);
+	return(0);
+}
+
+
